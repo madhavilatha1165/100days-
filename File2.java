623f5fd3a286333c647085a6bb336e60aa5e7cfc
madhavilatha1165 <70645508+madhavilatha1165@users.noreply.github.com>
2020-09-02 19:58:41 +0530

    Add files via upload

---
+++ File2.java
@@ -0,0 +1,14 @@
+package pack;
+import java.util.Scanner;
+public class File2 {
+	public static void main(String[] args) {
+		Scanner obj=new Scanner(System.in);
+		System.out.println("enter number 1:");
+		int a=obj.nextInt();
+		System.out.println("enter number 2:");
+		int b=obj.nextInt();
+		int c=a+b;
+		System.out.println("sum"+c);
+		}
+
+}
