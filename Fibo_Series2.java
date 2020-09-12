cf879f22407d79a9dc98ad1fd47dd063bc63ac4a
madhavilatha1165 <70645508+madhavilatha1165@users.noreply.github.com>
2020-09-12 18:34:04 +0530

    Add files via upload

--- Fibo_Series2.java
+++ Fibo_Series2.java
@@ -1,20 +1,21 @@
 package prgms3;
 import java.util.*;
-public class Fibo_Series {
+public class Fibo_Series2 {
 
 	public static void main(String[] args) {
 		Scanner obj = new Scanner(System.in);
 		System.out.println("enter number:");
 		int n = obj.nextInt();
-		int t1 = 0, t2 = 1;
-		System.out.println("Fibonacci series:");
-		System.out.println(t1+""+t2);
-		for(int i=2; i<n; i++)
+		int t1 = 0,t2 = 1;
+		System.out.println("Fibonacci numbers upto given number:"+n);
+		System.out.print(t1);
+		int sum = 0;
+		for(int i = 2;sum<n; i++)
 		{
-			int sum = t1 + t2;
-			System.out.println(""+sum);
+			sum = t1 + t2;
 			t1 = t2;
 			t2 = sum;
+			System.out.println(""+t1);
 		}
 
 	}
---
+++ Reverse_Digits.java
@@ -0,0 +1,22 @@
+package prgms3;
+import java.util.*;
+public class Reverse_Digits {
+
+	public static void main(String[] args) {
+		Scanner obj = new Scanner(System.in);
+		System.out.println("enter number:");
+		int n = obj.nextInt();
+		int fdigit,ldigit,digits,rev_num;
+		ldigit= n%10;
+		digits = (int)Math.log10(n);
+		fdigit = (int)(n/Math.pow(10, digits));
+		rev_num = ldigit;
+		rev_num = rev_num * (int)(Math.pow(10,digits));
+		rev_num = rev_num + n%(int)(Math.pow(10,digits));
+		rev_num = rev_num - ldigit;
+		rev_num  = rev_num + fdigit;
+		System.out.println("Reverse digit number ="+rev_num);
+
+	}
+
+}
