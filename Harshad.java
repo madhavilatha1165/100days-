9524aaaf6f3064c214b6370281405f64f5c74bb7
madhavilatha1165 <70645508+madhavilatha1165@users.noreply.github.com>
2020-09-22 19:02:10 +0530

    Add files via upload

---
+++ Harshad.java
@@ -0,0 +1,23 @@
+package prgms5;
+import java.util.*;
+public class Harshad {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("enter number:");
+		int n = sc.nextInt();
+		int sum = 0;
+		int c=n;
+		while(c>0)
+		{
+			int rem = c%10;
+			sum = sum + rem;
+			c=c/10;
+		}
+        if(n%sum==0)
+        	System.out.println("harshad number");
+        else
+        	System.out.println("not harshad number");
+	}
+
+}
