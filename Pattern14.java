55d59f5965032fe133275a0659a7e5f98c1dc3fb
madhavilatha1165 <70645508+madhavilatha1165@users.noreply.github.com>
2020-10-03 16:22:40 +0530

    Add files via upload

---
+++ Pattern14.java
@@ -0,0 +1,19 @@
+package patterns;
+
+public class Pattern14 {
+
+	public static void main(String[] args) {
+		char i;
+		char j;
+		for( i='A';i<='E';i++)
+        {
+        	for(j='A';j<=i;j++)
+        	{
+        		System.out.print(j);//A
+        	}
+        	System.out.println();
+        }
+
+	}
+
+}
