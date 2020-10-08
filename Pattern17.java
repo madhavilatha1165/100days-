33283c89db1b0d59ab2adf736a82a3cdb8e514a2
madhavilatha1165 <70645508+madhavilatha1165@users.noreply.github.com>
2020-10-08 20:01:58 +0530

    Add files via upload

---
+++ Pattern17.java
@@ -0,0 +1,14 @@
+package patterns2;
+public class Pattern17 {
+public static void main(String[] args) {
+	for(int i=5;i>=1;i--)//5 rows
+	{
+		for(int j=1;j<=i;j++)//5 cols
+		{
+			System.out.print(j);//1 2 3 4 5
+		}
+		System.out.println();
+	}
+}
+
+}
---
+++ Pattern18.java
@@ -0,0 +1,15 @@
+package patterns2;
+public class Pattern18 {
+public static void main(String[] args) {
+	 for(int i='A';i<='E';i++)
+	 {
+		 for(int j='E';j>=i;j--)
+		 {
+			 System.out.print((char)i+"");
+		 }
+		 System.out.println();//nextline
+	 }
+
+	}
+
+}
