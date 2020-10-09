88ec42e6f89ec5cbb865ea67406eb73a81889839
madhavilatha1165 <70645508+madhavilatha1165@users.noreply.github.com>
2020-10-09 15:45:01 +0530

    Add files via upload

---
+++ Pattern19.java
@@ -0,0 +1,15 @@
+package patterns2;
+public class Pattern19 {
+public static void main(String[] args) {
+	 for(int i='E';i>='A';i--)
+	 {
+		 for(int j='A';j<=i;j++)
+		 {
+			 System.out.print((char)j+"");
+		 }
+		 System.out.println();//nextline
+	 }	
+
+	}
+
+}
