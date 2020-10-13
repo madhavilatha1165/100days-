ef1f572f3ff81b9cee01664fe46a40df7cd59e6c
madhavilatha1165 <70645508+madhavilatha1165@users.noreply.github.com>
2020-10-13 13:38:28 +0530

    Add files via upload

---
+++ Pattern27.java
@@ -0,0 +1,15 @@
+package patterns2;
+public class Pattern27 {
+public static void main(String[] args) {
+		for(int i='A';i<='E';i++)
+		{
+			for(int j='E'-1;j>=i;j--)
+				System.out.print(" ");
+			for(int k='A';k<=i;k++)
+				System.out.print((char)i);
+			System.out.println();
+		}
+
+	}
+
+}
