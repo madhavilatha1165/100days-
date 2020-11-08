314a9e59d93a351bd4ea379029f9adf1a9eb0f91
madhavilatha1165 <70645508+madhavilatha1165@users.noreply.github.com>
2020-11-08 21:50:21 +0530

    Add files via upload

---
+++ Pattern80.java
@@ -0,0 +1,26 @@
+package patterns5;
+
+public class Pattern80 {
+
+	public static void main(String[] args) {
+		int n = 5;
+		int x = n; // x = 5
+		int y = n; // y = 5
+		
+		for(int i = 1; i <= n; i++) // i = 1,2,3,4,5
+		{
+			for(int j= 1; j<= (n*2)-1; j++)// j= 1,2,3,4,5,6,7,8,9
+			{
+				if(j==x || j==y) // 5==5||5==5 -> t
+					System.out.print(i);  //*
+				else
+					System.out.print(" ");
+			}
+			System.out.println();
+			x = x-1; // x = 4,3,2,1
+			y = y+1; // y = 6,7,8,9
+		}
+
+	}
+
+}
