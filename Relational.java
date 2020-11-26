c7dc1c8c459bb2f44963d825361576e246b223cf
madhavilatha1165 <70645508+madhavilatha1165@users.noreply.github.com>
2020-11-26 18:16:43 +0530

    Add files via upload

---
+++ Relational.java
@@ -0,0 +1,12 @@
+package string_prgms;
+
+public class Relational {
+
+	public static void main(String[] args) {
+		System.out.println(10<20);
+		System.out.println('a'<10);
+		System.out.println('a'>97.6);
+
+	}
+
+}
