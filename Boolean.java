05247689ba23a190a4896e6d4c15886d8d8acb1f
madhavilatha1165 <70645508+madhavilatha1165@users.noreply.github.com>
2020-11-28 22:45:47 +0530

    Add files via upload

---
+++ Boolean.java
@@ -0,0 +1,15 @@
+package string_prgms;
+
+public class Boolean {
+
+	public static void main(String[] args) {
+		boolean a=true;
+		boolean b=false;
+		System.out.println("a&b="+(a&b));
+		System.out.println("a|b="+(a|b));
+		System.out.println("a^b="+(a^b));
+		System.out.println("!a="+(!a));
+
+	}
+
+}
