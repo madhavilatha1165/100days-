9332d60296a9d65a2bc398f9306db50c7af5a866
madhavilatha1165 <70645508+madhavilatha1165@users.noreply.github.com>
2020-11-29 20:21:53 +0530

    Add files via upload

---
+++ Unary.java
@@ -0,0 +1,21 @@
+package string_prgms;
+
+public class Unary {
+
+	public static void main(String[] args) {
+		int x=4,y=4;
+		System.out.println(x);
+		System.out.println(x++);
+		System.out.println(x);
+		System.out.println(++x);
+		System.out.println(x);
+		System.out.println(y);
+		System.out.println(y--);
+		System.out.println(y);
+		System.out.println(--y);
+		System.out.println(y);
+		
+
+	}
+
+}
