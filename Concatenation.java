c441f62426ab5ed5bcc86a7d859ace52807ad1fe
madhavilatha1165 <70645508+madhavilatha1165@users.noreply.github.com>
2020-11-30 14:39:05 +0530

    Add files via upload

---
+++ Concatenation.java
@@ -0,0 +1,15 @@
+package string_prgms;
+
+public class Concatenation {
+
+	public static void main(String[] args) {
+		String s="Madhavi.Karri";
+		int a=10,b=20,c=30;
+		System.out.println(s+a+b+c);
+		System.out.println(a+b+s+c);
+		System.out.println(a+c+s+b);
+		System.out.println(a+s+b+c);
+
+	}
+
+}
