3dc78fb7f75a575d362f4e83337784bdf2e161c8
madhavilatha1165 <70645508+madhavilatha1165@users.noreply.github.com>
2020-12-06 19:14:57 +0530

    Add files via upload

---
+++ In_out2.java
@@ -0,0 +1,17 @@
+package hackerank_prgms;
+import java.util.*;
+public class In_out2 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int i = sc.nextInt();
+		double d = sc.nextInt();
+		sc.nextLine();
+		String s = sc.nextLine();
+		System.out.println("String: "+s);
+		System.out.println("Double: "+d);
+		System.out.println("Int: "+i);
+
+	}
+
+}
